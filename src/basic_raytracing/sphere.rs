0e7aa833391e9b390ce82d58d7484bc0185832ab
Thomas Gale <gale.thomasjames@gmail.com>
2022-05-23 20:22:24 +0100

    In think I finally understand my byte aligment issues.
    It was further complicated by sending an array of Spheres, meaning that I needed to ensure internal byte aligment and also that the array met the byte aligment rules (scalar -> 4, vec2 -> 8 and vec3/vec4 -> 16).

--- src/basic_raytracing/sphere.rs
+++ src/basic_raytracing/sphere.rs
@@ -4,9 +4,9 @@ pub struct Sphere {
     pub center: [f32; 3],
     pub _pad1: f32,         // Byte 16
     pub albedo: [f32; 3],   // Coloration of the ray bounce
-    pub _pad2: f32,         // Byte 32
     pub material_type: u32, // 0: lambertian, 1: metal, 2: dielectric
     pub radius: f32,
     pub fuzz: f32,  // Roughness for metals
+    pub _pad2: f32, 
     pub _pad3: f32, // Byte 48
 }
